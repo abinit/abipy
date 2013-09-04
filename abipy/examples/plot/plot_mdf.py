2573ff16d6ce17ffda559eccef8bea542ef53910
Yannick Gillet <yanikou19@gmail.com>
2013-09-04 16:24:49 +0200

    Dielectric tensor computation from a MDF_File

--- abipy/examples/plot/plot_mdf.py
+++ abipy/examples/plot/plot_mdf.py
@@ -12,3 +12,9 @@ mdf_file = abiopen(data.ref_file("tbs_4o_DS2_MDF.nc"))
 # dielectric function (EXC, RPA, GWRPA) betwee 2 and 5 eV.
 title = "Si absorption spectrum: EXC vs RPA"
 mdf_file.plot_mdfs(title=title, xlim=(2, 5))
+
+# Plot the 4 components of the macroscopic dielectric tensor
+title = "Si macroscopic dielectric tensor"
+tensor_exc = mdf_file.get_tensor("exc")
+tensor_exc.plot(title=title)
+
