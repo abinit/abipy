2e5dab7ebae4d0e627bdbb9da03ecb5228b74661
Yannick Gillet <yanikou19@gmail.com>
2013-09-20 13:56:09 +0200

    Symmetrization of the tensor using symmetries of the space group.

--- abipy/core/tensor.py
+++ abipy/core/tensor.py
@@ -1,10 +1,13 @@
 """
-TODO
+This module contains classes representing tensors and helper functions to change lattice.
 """
 from __future__ import print_function, division
 
 import itertools
 import numpy as np
+
+from abipy.core import Structure
+from pymatgen.symmetry.finder import SymmetryFinder
 
 __all__ = [
     "Tensor",
@@ -12,9 +15,14 @@ __all__ = [
 ]
 
 
+def from_cart_to_red(cartesian_tensor,lattice):
+    mat = lattice.inv_matrix
+    red_tensor = np.dot(np.dot(np.transpose(mat), cartesian_tensor), mat)
+    return red_tensor
+
 class Tensor(object):
     """Representation of a 3x3 tensor"""
-    def __init__(self, red_tensor, lattice):
+    def __init__(self, red_tensor, lattice, space="r"):
         """
         Args:
             red_tensor:
@@ -24,39 +32,60 @@ class Tensor(object):
         """
         self._reduced_tensor = red_tensor
         self._lattice = lattice
+      
+        if space == "g":
+            self._is_real_space = False
+        elif space == "r":
+            self._is_real_space = True
+        else:
+            raise AttributeError("space should be either 'g' or 'r'")
 
     @property
     def reduced_tensor(self):
         return self._reduced_tensor
 
     @property
-    def cartesian_tensor(self):
-        try:
-            return self._cartesian_tensor
-        except AttributeError:
-            mat = self._lattice.matrix
-            self._cartesian_tensor = np.dot(np.dot(np.transpose(mat),self._reduced_tensor),mat)
-        
-        return self._cartesian_tensor
+    def is_real_space(self):
+        return self._is_real_space
 
+    @property
+    def cartesian_tensor(self):
+        mat = self._lattice.matrix
+        return np.dot(np.dot(np.transpose(mat),self._reduced_tensor),mat)
 
     @classmethod
-    def from_cartesian_tensor(cls, cartesian_tensor,lattice):
-        mat = lattice.inv_matrix
-        red_tensor = np.dot(np.dot(np.transpose(mat), cartesian_tensor), mat)
-        return cls(red_tensor, lattice)
+    def from_cartesian_tensor(cls, cartesian_tensor,lattice,space="r"):
+        red_tensor = from_cart_to_red(cartesian_tensor,lattice)
+        return cls(red_tensor, lattice,space)
 
-    def symmetrize(self, pointgroup, system="reciprocal_lattice"):
+    def symmetrize(self):
         tensor = self._reduced_tensor
 
-        if system == "reciprocal_lattice":
-            real_lattice = self._lattice.reciprocal_lattice
-        elif system == "real_lattice":
+        if self._is_real_space:
             real_lattice = self._lattice
         else:
-            raise AttributeError("system not understood")
+            real_lattice = self._lattice.reciprocal_lattice
 
-        # TODO symmetrize
+        # Create a fake structure to get point group
+        real_structure = Structure(real_lattice,["H"],[[0,0,0]])
+        real_finder = SymmetryFinder(real_structure)
+
+        real_symmops = real_finder.get_point_group_operations(cartesian=True)
+
+        cartesian_tensor = self.cartesian_tensor
+
+        sym_tensor = np.zeros((3,3))
+
+        my_tensor = cartesian_tensor
+
+        for real_sym in real_symmops:
+             mat = real_sym.rotation_matrix
+             prod_sym = np.dot(np.transpose(mat),np.dot(cartesian_tensor,mat))
+             sym_tensor = sym_tensor + prod_sym
+
+        sym_tensor = sym_tensor/len(real_symmops)
+
+        self._reduced_tensor = from_cart_to_red(sym_tensor,self._lattice)
 
 class SymmetricTensor(Tensor):
     """Representation of a 3x3 symmetric tensor"""
--- abipy/core/tests/test_tensor.py
+++ abipy/core/tests/test_tensor.py
@@ -4,7 +4,9 @@ from __future__ import print_function, division
 import numpy as np
 import abipy.data as data
 
+from pymatgen.core.structure import Structure
 from pymatgen.core.lattice import Lattice
+from pymatgen.symmetry.finder import SymmetryFinder
 from abipy.core.tensor import *
 from abipy.core.testing import *
 
@@ -14,13 +16,30 @@ class TestTensor(AbipyTest):
     def test_tensor(self):
         """Initialize Tensor"""
 
-        lattice = Lattice.monoclinic(4,5,6,74)
+        lattice = Lattice.hexagonal(4,6)
+        #rprimd = np.array([[0,0.5,0.5],[0.5,0,0.5],[0.5,0.5,0]])
+        #rprimd = rprimd*10
+        #lattice = Lattice(rprimd)
+        structure = Structure(lattice, ["Ga", "As"],
+                                      [[0, 0, 0], [0.5, 0.5, 0.5]])
 
-        cartesian_tensor = [[2,3,0],[3,4,0],[0,0,6]]
-        tensor = Tensor.from_cartesian_tensor(cartesian_tensor,lattice.reciprocal_lattice)
+        finder = SymmetryFinder(structure)
+
+        spacegroup = finder.get_spacegroup()
+        pointgroup = finder.get_point_group()
+
+        cartesian_tensor = [[2,3,1.2],[3,4,1.0],[1.2,1.0,6]]
+
+        tensor = Tensor.from_cartesian_tensor(cartesian_tensor,lattice.reciprocal_lattice,space="g")
         red_tensor = tensor.reduced_tensor
-        tensor2 = Tensor(red_tensor,lattice.reciprocal_lattice)
+        tensor2 = Tensor(red_tensor,lattice.reciprocal_lattice,space="g")
         assert(((np.abs(tensor2.cartesian_tensor)-np.abs(cartesian_tensor)) < 1E-8).all())
+
+        #print("non-symmetrized cartesian_tensor = ",tensor2.cartesian_tensor)
+        tensor2.symmetrize()
+
+        #print("symmetrized_cartesian_tensor = ",tensor2.cartesian_tensor)
+
 
 if __name__ == "__main__":
     import unittest
