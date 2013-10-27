d16ac2c00f3c37b8d3a741dbe6fc1ec479965e31
gmatteo <gmatteo@gmail.com>
2013-10-27 18:33:27 +0100

    Add new examples for LDA+U, MgB2 and bands with frozen phonon at q=0.

---
+++ abipy/data/runs/run_mgb2_edoses.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+"""
+This script computes the band structure and the electron DOS of MgB2
+with different k-point samplings.
+"""
+from __future__ import division, print_function
+
+import os
+import abipy.data as data  
+import abipy.abilab as abilab
+
+from abipy.data.runs import Tester, enable_logging
+from abipy.core.testing import AbipyTest
+
+
+def make_scf_nscf_inputs(structure, pseudos):
+
+    inp = abilab.AbiInput(pseudos=pseudos, ndtset=5)
+    inp.set_structure(structure)
+
+    # Global variables
+    global_vars = dict(ecut=10,
+                       nband=11,
+                       timopt=-1,
+                       occopt=4,    # Marzari smearing
+                       tsmear=0.03,
+                    )
+
+    inp.set_variables(**global_vars)
+
+    # Dataset 1 (GS run)
+    inp[1].set_kmesh(ngkpt=[8,8,8], 
+                    shiftk=abilab.shiftk_from_structure(structure)
+                   )
+
+    inp[1].set_variables(tolvrs=1e-6)
+
+    # Dataset 2 (NSCF Band Structure)
+    inp[2].set_kpath(ndivsm=6)
+    inp[2].set_variables(tolwfr=1e-12)
+
+    # Dos calculations
+    for i, nksmall in enumerate([10, 14, 18]):
+        inp[i+3].set_variables(
+            iscf=-3,   # NSCF calculation
+            ngkpt=structure.calc_ngkpt(nksmall),      
+            shiftk=[0.0, 0.0, 0.0],
+            tolwfr=1.0e-10,
+        )
+    
+    # return GS, NSCF (band structure), DOSes input.
+    return  inp.split_datasets()
+
+
+def build_bands_flow(workdir):
+    pseudos = data.pseudos("12mg.pspnc", "05b.soft_tm")
+    structure = data.structure_from_ucell("MgB2")
+
+    from abipy.htc.input import nvalence
+    nval = nvalence(structure, pseudos)
+    print(nval)
+
+    inputs = make_scf_nscf_inputs(structure, pseudos)
+    scf_input, nscf_input, dos_inputs = inputs[0], inputs[1], inputs[2:]
+    #print(scf_input.pseudos)
+
+    manager = abilab.TaskManager.from_user_config()
+                                                               
+    flow = abilab.bandstructure_flow(workdir, manager, scf_input, nscf_input, dos_inputs=dos_inputs)
+
+    return flow.allocate()
+
+@enable_logging
+def main():
+    tester = Tester()
+    flow = build_bands_flow(tester.workdir)
+    return flow.build_and_pickle_dump()
+
+
+if __name__ == "__main__":
+    import sys
+    sys.exit(main())
