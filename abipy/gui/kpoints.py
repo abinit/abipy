7d5dbff58dfb10ccdfacd34a4ce46aee9b4bb650
gmatteo <gmatteo@gmail.com>
2013-12-07 01:27:11 +0100

    New version of KpointsPanel

--- abipy/gui/kpoints.py
+++ abipy/gui/kpoints.py
@@ -7,8 +7,74 @@ import wx.lib.mixins.listctrl as listmix
 
 from collections import OrderedDict
 
-class KpointsPanel(awx.Panel):
 
+class KpointsListCtrl(wx.ListCtrl, listmix.ColumnSorterMixin):
+    """
+    ListCtrl containing k-points. Support column sorting.
+    """
+    def __init__(self, parent, kpoints, **kwargs):
+        """
+        Args:
+            parent:
+                Parent window.
+            kpoints:
+                List of `Kpoint` instances.
+        """
+        super(KpointsListCtrl, self).__init__(parent, id=-1, style=wx.LC_REPORT | wx.BORDER_SUNKEN, **kwargs)
+
+        self.kpoints = kpoints
+
+        columns = ["#", 'Reduced coordinates', 'Weight', 'Name']
+
+        for (index, col) in enumerate(columns):
+            self.InsertColumn(index, col)
+
+        # Used to store the Max width in pixels for the data in the column.
+        column_widths = [awx.get_width_height(self, s)[0] for s in columns]
+
+        # Used by the ColumnSorterMixin, see wx/lib/mixins/listctrl.py
+        self.itemDataMap = {}
+
+        for (index, kpt) in enumerate(self.kpoints):
+            entry = ["%d\t\t" % index, 
+                     "[%.5f,  %.5f,  %.5f]\t\t" % tuple(c for c in kpt.frac_coords), 
+                     "%.3f\t\t" % kpt.weight, 
+                     "%s" % kpt.name,
+                     ]
+            self.Append(entry)
+            self.itemDataMap[index] = entry
+            self.SetItemData(index, index)
+
+            w = [awx.get_width_height(self, s)[0] for s in entry]
+            column_widths = map(max, zip(w, column_widths))
+
+        for (index, col) in enumerate(columns):
+            self.SetColumnWidth(index, column_widths[index])
+
+        # Now that the list exists we can init the other base class, see wx/lib/mixins/listctrl.py
+        listmix.ColumnSorterMixin.__init__(self, len(columns))
+
+    def GetListCtrl(self):
+        """Used by the ColumnSorterMixin, see wx/lib/mixins/listctrl.py"""
+        return self
+
+    def getSelectedKpoint(self):
+        """
+        Returns the kpoint selected by the user.
+        None if no selection has been done.
+        """
+        # Get selected index, map to index in kpoints and return the kpoint.
+        item = self.GetFirstSelected()
+        if item == -1: return None
+        index = self.GetItemData(item)
+        return self.kpoints[index]
+
+
+class KpointsPanel(awx.Panel):
+    """
+    A panel with a list of k-points and a structure. 
+    Provides popup menus for inspecting the k-points.
+    """
     def __init__(self, parent, structure, kpoints, **kwargs):
         """
         Args:
@@ -21,20 +87,58 @@ class KpointsPanel(awx.Panel):
         """
         super(KpointsPanel, self).__init__(parent, **kwargs)
 
-        self.kpoints_listctrl = KpointsListCtrl(self, kpoints)
+        self.klist_ctrl = KpointsListCtrl(self, kpoints)
         self.structure = structure
 
         # Connect the events whose callback will be set by the client code.
-        self.kpoints_listctrl.Bind(wx.EVT_LIST_ITEM_RIGHT_CLICK, self.OnRightClick)
+        self.klist_ctrl.Bind(wx.EVT_LIST_ITEM_RIGHT_CLICK, self.onRightClick)
 
         main_sizer = wx.BoxSizer(wx.VERTICAL)
-        main_sizer.Add(self.kpoints_listctrl, 1, wx.ALL | wx.EXPAND | wx.ALIGN_CENTER_VERTICAL, 5)
+        main_sizer.Add(self.klist_ctrl, 1, wx.ALL | wx.EXPAND | wx.ALIGN_CENTER_VERTICAL, 5)
         self.SetSizerAndFit(main_sizer)
 
-    def OnRightClick(self, event):
-        popmenu = KpointPopupMenu(self, structure=self.structure, kpoint=self.kpoints_listctrl.GetKpoint())
-        self.PopupMenu(popmenu, event.GetPoint())
-        popmenu.Destroy()
+    def onRightClick(self, event):
+        """Generate the popup menu."""
+        kpoint = self.klist_ctrl.getSelectedKpoint()
+
+        popup_menu = self.makePopupMenu()
+        self.PopupMenu(popup_menu, event.GetPoint())
+        popup_menu.Destroy()
+
+    def makePopupMenu(self):
+        """
+        Build and return a popup menus so that subclasses can extend 
+        or replace this base method.
+        """
+        self.ID_POPUP_LITTLEGROUP = wx.NewId()
+        self.ID_POPUP_STAR = wx.NewId()
+
+        menu = wx.Menu()
+        menu.Append(self.ID_POPUP_LITTLEGROUP, "Little group")
+        menu.Append(self.ID_POPUP_STAR, "Show star")
+
+        # Associate menu/toolbar items with their handlers.
+        menu_handlers = [
+            (self.ID_POPUP_LITTLEGROUP, self.onLittleGroup),
+            (self.ID_POPUP_STAR, self.onShowStar),
+        ]
+                                                            
+        for combo in menu_handlers:
+            mid, handler = combo[:2]
+            self.Bind(wx.EVT_MENU, handler, id=mid)
+                                                     
+        return menu
+
+    def onLittleGroup(self, event):
+        kpoint = self.klist_ctrl.getSelectedKpoint()
+        ltk = self.structure.spacegroup.find_little_group(kpoint)
+        table, header = ltk.bilbao_character_table()
+        awx.SimpleGridFrame(self, table, title=header, labels_from_table=True).Show()
+
+    def onShowStar(self, event):
+        kpoint = self.klist_ctrl.getSelectedKpoint()
+        star = kpoint.compute_star(self.structure.fm_symmops)
+        KpointsFrame(self, self.structure, star, title="Star of point: " + str(star.base_point)).Show()
 
 
 class KpointsFrame(awx.Frame):
@@ -51,81 +155,6 @@ class KpointsFrame(awx.Frame):
         super(KpointsFrame, self).__init__(parent, **kwargs)
 
         panel = KpointsPanel(self, structure, kpoints)
-
-
-##################
-### Callbacks ###
-##################
-
-
-def showLittleGroup(parent, structure, kpoint):
-    ltk = structure.spacegroup.find_little_group(kpoint)
-    table, header = ltk.bilbao_character_table()
-    awx.SimpleGridFrame(parent, table, title=header, labels_from_table=True).Show()
-
-
-def showStar(parent, structure, kpoint):
-    star = kpoint.compute_star(structure.fm_symmops)
-    #wxdg.ScrolledMessageDialog(parent, str(star), caption="Star of the kpoint", style=wx.MAXIMIZE_BOX).Show()
-    KpointsFrame(parent, structure, star).Show()
-
-
-class KpointPopupMenu(wx.Menu):
-    """
-    Base class for popup menus. `A KpointPopupMenu` has a list of callback functions
-    indexed by the menu title. The signature of the callback function is func(parent, *args, **kwargs) 
-    parent is the wx Window that will become the parent of the new frame created by the callback.
-    """
-    MENU_TITLES = OrderedDict([
-        ("Little Group", showLittleGroup),
-        ("Star", showStar),
-    ])
-
-    def __init__(self, parent, structure, kpoint):
-        """
-        Args:
-            parent:
-                Parent window.
-            structure:
-                `Structure` object
-            kpoint:
-                `Kpoint` object of vector with the reduced coordinates.
-        """
-        super(KpointPopupMenu, self).__init__()
-        self._parent, self.structure, self.kpoint = parent, structure, kpoint
-
-        self._make_menu()
-
-    def _make_menu(self):
-        """Build the menu taking into account the options of the superclasses."""
-
-        assert not hasattr(self, "menu_title_by_id")
-        assert not hasattr(self, "menu_titles")
-        self.menu_title_by_id, self.menu_titles = OrderedDict(), OrderedDict()
-
-        for title in self.MENU_TITLES:
-            self.menu_title_by_id[wx.NewId()] = title
-
-        for (id, title) in self.menu_title_by_id.items():
-            # Register menu handlers with EVT_MENU, on the menu.
-            self.Append(id, title)
-            wx.EVT_MENU(self, id, self.OnMenuSelection)
-
-    @property
-    def parent(self):
-        """Returns the parent window."""
-        return self._parent
-
-    def OnMenuSelection(self, event):
-        """Call the callback selected in the popupmenu."""
-        title = self.menu_title_by_id[event.GetId()]
-        callback = self.MENU_TITLES[title]
-
-        try:
-            callback(parent=self.parent, structure=self.structure, kpoint=self.kpoint)
-        except:
-            awx.showErrorMessage(parent=self.parent)
-
 
 
 class SpinKpointBandPanel(awx.Panel):
@@ -179,9 +208,9 @@ class SpinKpointBandPanel(awx.Panel):
         kpoint_label.Wrap(-1)
         hsizer2.Add(kpoint_label, 0, wx.ALIGN_CENTER_VERTICAL | wx.ALL, 5)
 
-        self.kpoint_listctrl = klist = KpointsListCtrl(self, self.kpoints)
+        self.klist_ctrl = klist = KpointsListCtrl(self, self.kpoints)
 
-        hsizer2.Add(self.kpoint_listctrl, 1, wx.ALL | wx.EXPAND | wx.ALIGN_CENTER_VERTICAL, 5)
+        hsizer2.Add(self.klist_ctrl, 1, wx.ALL | wx.EXPAND | wx.ALIGN_CENTER_VERTICAL, 5)
         main_sizer.Add(hsizer2, 1, wx.EXPAND | wx.ALIGN_CENTER_HORIZONTAL, 5)
 
         self.SetSizerAndFit(main_sizer)
@@ -194,7 +223,7 @@ class SpinKpointBandPanel(awx.Panel):
     def GetSKB(self):
         """Returns the tuple (spin, kpoint, band) selected by the user."""
         spin = int(self.spin_cbox.GetValue())
-        kpoint = self.kpoint_listctrl.GetKpoint()
+        kpoint = self.klist_ctrl.getSelectedKpoint()
         band = int(self.band_cbox.GetValue())
         return spin, kpoint, band
 
@@ -241,63 +270,63 @@ class SpinKpointBandPanel(awx.Panel):
             self._on_item_activated_callback(*skb)
 
 
-class KpointsListCtrl(wx.ListCtrl, listmix.ColumnSorterMixin):
-    """
-    ListCtrl with info on the k-points. Support Column sorting.
-    """
-    def __init__(self, parent, kpoints, **kwargs):
-        """
-        Args:
-            parent:
-                Parent window.
-            kpoints:
-                List of `Kpoint` instances.
-        """
-        super(KpointsListCtrl, self).__init__(parent, id=-1, style=wx.LC_REPORT | wx.BORDER_SUNKEN, **kwargs)
 
-        self.kpoints = kpoints
 
-        columns = ["#", 'Reduced coordinates', 'Weight', 'Name']
 
-        for (index, col) in enumerate(columns):
-            self.InsertColumn(index, col)
 
-        # Used to store the Max width in pixels for the data in the column.
-        column_widths = [awx.get_width_height(self, s)[0] for s in columns]
 
-        # Used by the ColumnSorterMixin, see wx/lib/mixins/listctrl.py
-        self.itemDataMap = {}
 
-        for (index, kpt) in enumerate(self.kpoints):
-            entry = ["%d\t\t" % index, 
-                     "[%.5f,  %.5f,  %.5f]\t\t" % tuple(c for c in kpt.frac_coords), 
-                     "%.3f\t\t" % kpt.weight, 
-                     "%s" % kpt.name,
-                     ]
-            self.Append(entry)
-            self.itemDataMap[index] = entry
-            self.SetItemData(index, index)
 
-            w = [awx.get_width_height(self, s)[0] for s in entry]
-            column_widths = map(max, zip(w, column_widths))
 
-        for (index, col) in enumerate(columns):
-            self.SetColumnWidth(index, column_widths[index])
 
-        # Now that the list exists we can init the other base class, see wx/lib/mixins/listctrl.py
-        listmix.ColumnSorterMixin.__init__(self, len(columns))
 
-    def GetListCtrl(self):
-        """Used by the ColumnSorterMixin, see wx/lib/mixins/listctrl.py"""
-        return self
 
-    def GetKpoint(self):
-        """
-        Returns the kpoint selected by the user.
-        None if no selection has been done.
-        """
-        # Get selected index, map to index in kpoints and return the kpoint.
-        item = self.GetFirstSelected()
-        if item == -1: return None
-        index = self.GetItemData(item)
-        return self.kpoints[index]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
