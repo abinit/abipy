dcd5bf04d6456b075bee1d6a1098cc00633442d6
gmatteo <gmatteo@gmail.com>
2013-12-16 22:12:42 +0100

    Rename gw_kbpolicy to gw_qprange

--- abipy/data/runs/run_ht_si_g0w0ppm.py
+++ abipy/data/runs/run_ht_si_g0w0ppm.py
@@ -62,7 +62,7 @@ def build_flow(options):
 
     work = g0w0_with_ppmodel(structure, pseudos, scf_kppa, nscf_nband, ecuteps, ecutsigx,
                              accuracy="normal", spin_mode="unpolarized", smearing=None, 
-                             ppmodel="godby", charge=0.0, inclvkb=2, sigma_nband=None, gw_kbpolicy=1,
+                             ppmodel="godby", charge=0.0, inclvkb=2, sigma_nband=None, gw_qprange=1,
                              scr_nband=None, **extra_abivars)
     
     flow.register_work(work)
