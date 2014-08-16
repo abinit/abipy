ea931c3618cd5123d215aa2af80ab1f3e42eaef3
gmatteo <gmatteo@gmail.com>
2014-08-16 20:03:04 +0200

    Change extension (dat --> in)

--- abipy/gui/oncv_data/missing.py
+++ abipy/gui/oncv_data/missing.py
@@ -3,9 +3,14 @@ import os
 
 from pymatgen import periodic_table
 
+input_files = [f for f in os.listdir(".") if f.endswith(".dat")]
+
+#for f in input_files:
+#    os.rename(f, f.replace(".dat", ".in"))
+
 input_files = [f.split(".dat")[0] for f in os.listdir(".") if f.endswith(".dat")]
 symbols_done = [f.split("-")[0] for f in input_files]
-#print("done:\n", symbols_done, "\nend done")
+print("done:\n", symbols_done, "\nend done")
 
 for s in periodic_table.all_symbols():
   if s not in symbols_done:
