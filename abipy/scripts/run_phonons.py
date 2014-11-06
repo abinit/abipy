b31b3e8c19338c158b679a86ca78bce3bb252b7c
setten <setten@gmail.com>
2014-11-06 13:24:47 +0100

    phonon script

--- abipy/scripts/run_phonons.py
+++ abipy/scripts/run_phonons.py
@@ -172,7 +172,7 @@ def build_flow(structure, workdir, options):
     all_inps = scf_ph_inputs(structure, options)
     scf_input, ph_inputs = all_inps[0], all_inps[1:]
 
-    return abilab.phonon_flow(workdir, manager, scf_input, ph_inputs, with_nscf=True, with_ddk=True)
+    return abilab.phonon_flow(workdir, manager, scf_input, ph_inputs, with_nscf=True, with_ddk=False)
 
 
 class NotReady(Exception):
