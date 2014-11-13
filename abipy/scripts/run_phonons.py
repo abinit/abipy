92f5fc2e4d58a0e75134b63dc67fdb6b7bd933ba
setten <setten@gmail.com>
2014-11-13 11:52:48 +0100

    tests

--- abipy/scripts/run_phonons.py
+++ abipy/scripts/run_phonons.py
@@ -57,7 +57,7 @@ def scf_ph_inputs(structure, options):
     qpoints = np.reshape(qpoints, (-1, 3))
     qpoints = unique_rows(np.concatenate((qpoints, qptbounds), axis=0))
 
-    qpoints = [[0.0, 0.0, 0.0]]
+    #qpoints = [[0.0, 0.0, 0.0]]
 
     # Global variables used both for the GS and the DFPT run.
     global_vars = dict(ecut=16.0,
