d161bb38752f71d46fae6ac1402507f177e5145f
gmatteo <gmatteo@gmail.com>
2014-11-14 17:08:07 +0100

    Move open_hook to dev_scripts because it's not compatible with py3k

--- dev_scripts/open_hook.py
+++ dev_scripts/open_hook.py
@@ -103,4 +103,45 @@ def remove():
     __builtin__.open = _builtin_open = __builtin__.open
 
 
+def test_open_hook():
+    """Unit tests for open_hook"""
+    import unittest
+    import tempfile
 
+    py3k = False
+    try:
+        import __builtin__
+    except ImportError:
+        py3k = True
+        import builtins as __builtin__
+
+
+    @unittest.skipIf(py3k, "OpenHook not compatible with py3k")
+    class OpenHookTest(unittest.TestCase):
+        """Test open_hook module."""
+        def setUp(self):
+            from abipy.tools import open_hook
+            self.open_hook = open_hook
+            self.open_hook.install()
+
+        def test_open(self):
+            """Test if open_hook detects open files."""
+            self.assertEqual(self.open_hook.num_openfiles(), 0)
+
+            _, fname = tempfile.mkstemp()
+            fh = open(fname)
+
+            self.open_hook.print_open_files()
+            self.assertEqual(self.open_hook.num_openfiles(), 1)
+
+        def tearDown(self):
+            self.open_hook.remove()
+            # Here we should have the __builtin__ version
+
+            self.assertTrue(file is __builtin__.file)
+            self.assertTrue(open is __builtin__.open)
+
+
+if __name__ == "__main__":
+    import unittest
+    unittest.main()
