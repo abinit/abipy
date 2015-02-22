6149a59e8bafd5c3e2cfc480493d6db5216f4055
Michiel van Setten <mjvansetten@gmail.com>
2015-02-22 19:49:06 +0100

    lessons

--- abipy/lessons/lesson_kpoint_convergence.py
+++ abipy/lessons/lesson_kpoint_convergence.py
@@ -285,7 +285,7 @@ class Lesson(BaseLesson):
 
     @property
     def abipy_string(self):
-        return __doc__ + _ipython_lesson_ + _commandline_lesson_ 
+        return __doc__ + _ipython_lesson_
 
     @property
     def comline_string(self):
