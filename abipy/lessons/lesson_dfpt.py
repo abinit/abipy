31abd4f6c9b20ff4d7d3b3a847ac8bd045411d1f
gmatteo <gmatteo@gmail.com>
2015-04-06 21:33:07 +0200

    ipython notebooks

--- abipy/lessons/lesson_dfpt.py
+++ abipy/lessons/lesson_dfpt.py
@@ -9,10 +9,17 @@ import abipy.abilab as abilab
 import abipy.data as abidata  
 
 
-def make_scf_input(ecut=2, paral_kgb=0):
+def make_scf_input(ecut=2, ngkpt=(4, 4, 4)):
     """
-    This function constructs the input files for the phonon calculation: 
-    GS input + the input files for the phonon calculation.
+    This function constructs an `AbinitInput` for performing a 
+    GS-SCF calculation for crystalline AlAs.
+
+    Args:
+        ecut: cutoff energy in Ha
+        ngkpt: 3 integers specifying the k-mesh.
+
+    Return:
+        `AbinitInput` object 
     """
     # Crystalline AlAs: computation of the second derivative of the total energy
     gs_inp = abilab.AbinitInput(structure=abidata.structure_from_ucell("AlAs"),
@@ -21,14 +28,12 @@ def make_scf_input(ecut=2, paral_kgb=0):
     gs_inp.set_vars(
         nband=4,             
         ecut=ecut,         
-        ngkpt=[4, 4, 4],
+        ngkpt=ngkpt,
         nshiftk=4,
         shiftk=[0.0, 0.0, 0.5,   # This gives the usual fcc Monkhorst-Pack grid
                 0.0, 0.5, 0.0,
                 0.5, 0.0, 0.0,
                 0.5, 0.5, 0.5],
-        #shiftk=[0, 0, 0],
-        paral_kgb=paral_kgb,
         ixc=1,
         nstep=25,
         diemac=9.0,
@@ -38,29 +43,3 @@ def make_scf_input(ecut=2, paral_kgb=0):
     gs_inp.set_mnemonics(True)
     return gs_inp
 
-
-#@abilab.flow_main
-#def main(options):
-#    qpoints = np.reshape([
-#             0.00000000E+00,  0.00000000E+00,  0.00000000E+00, 
-#             2.50000000E-01,  0.00000000E+00,  0.00000000E+00,
-#    ], (-1, 3))
-#
-#    #return make_phq_ecutconv_flow(workdir="flow_alas_qconv", qpt=qpoints[0])
-#
-#    scf_input = make_scf_input(ecut=2, paral_kgb=0)
-#
-#    from pymatgen.io.abinitio.flows import phonon_conv_flow
-#    flow = phonon_conv_flow("flow_conv", scf_input, qpoints[0], 
-#        params=["ecut", [1, 2, 3]]
-#        #params=["tsmear", "ngkpt", [0.002, 0.003, 0.004], [[2, 2, 2], [4, 4, 4], [6, 6, 6]]],
-#    )
-#    from pymatgen.io.abinitio.flows import PhononFlow
-#    flow = PhononFlow.from_scf_input("flow_alas_phonons_and_becs", scf_input, ph_ngqpt=(4,4,4))
-#
-#    flow.build_and_pickle_dump()
-#    return flow
-#
-#
-#if __name__ == "__main__":
-#    sys.exit(main())
