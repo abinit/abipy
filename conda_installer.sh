f9987675e4fa08ab8b795315bab0b7ceedba9af4
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2017-03-16 16:54:10 +0100

    Debuggin osx on travis

--- conda_installer.sh
+++ conda_installer.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
+set -ev  # exit on first error, print each command
 
 # Install conda with travis: https://conda.io/docs/travis.html
 
 # We do this conditionally because it saves us some downloading if the version is the same.
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
+if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
+    if [[ "${TRAVIS_PYTHON_VERSION}" == "2.7" ]]; then
         curl -o miniconda.sh https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh;
     else
         curl -o miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh;
     fi
 else
-    if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
+    if [[ "${TRAVIS_PYTHON_VERSION}" == "2.7" ]]; then
         wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
     else
         wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
     fi
 fi
 
-bash miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
+bash miniconda.sh -b -p ${HOME}/miniconda
+export PATH="${HOME}/miniconda/bin:${PATH}"
 
 hash -r
 
@@ -28,5 +29,5 @@ conda update -q conda
 conda info -a
 # Replace dep1 dep2 ... with your dependencies
 # conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION dep1 dep2 ...
-conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION
+conda create -q -n test-environment python=${TRAVIS_PYTHON_VERSION}
 source activate test-environment
