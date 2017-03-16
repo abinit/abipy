baa9969b4e1ab0ecf380a6b1758833024957ab32
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2017-03-16 15:05:22 +0100

    Test osx

--- conda_installer.sh
+++ conda_installer.sh
@@ -3,10 +3,18 @@
 # Install conda with travis: https://conda.io/docs/travis.html
 
 # We do this conditionally because it saves us some downloading if the version is the same.
-if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
-  wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
+        curl -o miniconda.sh https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh;
+    else
+        curl -o miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh;
+    fi
 else
-  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+    if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
+        wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
+    else
+        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+    fi
 fi
 
 bash miniconda.sh -b -p $HOME/miniconda
