c35d15b62f9ac3fae95bbceeb19263e3b37837c7
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2017-03-21 23:20:06 +0100

    Test scripts in benchmarks

---
+++ abipy/benchmarks/test_scripts.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""
+This script runs all the python scripts located in this directory 
+"""
+from __future__ import print_function, division, unicode_literals, absolute_import
+
+import os 
+
+from subprocess import call, Popen
+
+from abipy.core.testing import *
+
+
+root = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+
+
+class TestScripts(AbipyTest):
+    def test_all_scripts(self):
+        """Testing all scripts in abipy/benckmarks"""
+        retcode = call(os.path.join(root, "_runemall.py"))
+        if retcode != 0: print(retcode, "scripts in ~abipy/data/runs exited with non-zero return code")
+        assert retcode == 0
