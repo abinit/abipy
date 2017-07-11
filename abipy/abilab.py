cd88a2ec16d8edde9f924e448935cd9e8ef2a9b4
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2017-07-11 22:32:09 +0200

    Fix import

--- abipy/abilab.py
+++ abipy/abilab.py
@@ -49,7 +49,7 @@ from abipy.electrons.gw import SigresFile, SigresPlotter
 from abipy.electrons.bse import MdfFile
 from abipy.electrons.scissors import ScissorsBuilder
 from abipy.electrons.scr import ScrFile
-from abipy.electrons.denpot import (DensityNcFile, VhartreeNcFile, VxcNcFile, VhxcNcFile, PotNcFile, DensityFortranFile
+from abipy.electrons.denpot import (DensityNcFile, VhartreeNcFile, VxcNcFile, VhxcNcFile, PotNcFile, DensityFortranFile,
     Cut3dDenPotNcFile)
 from abipy.electrons.fatbands import FatBandsFile
 from abipy.electrons.fold2bloch import Fold2BlochNcfile
