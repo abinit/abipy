8ebcf7df40701bf224fdd309d459db1f38baec4e
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2017-09-24 20:38:10 +0200

    Remove tests for seaborn interactplot (removed upstream)

--- abipy/display/tests/test_seabornw.py
+++ abipy/display/tests/test_seabornw.py
@@ -44,13 +44,13 @@ class SeabornWidgetTest(AbipyTest):
         snw.clustermap(flights)
 
         # Generate a random dataset with strong simple effects and an interaction
-        n = 80
-        rs = np.random.RandomState(11)
-        x1 = rs.randn(n)
-        x2 = x1 / 5 + rs.randn(n)
-        b0, b1, b2, b3 = .5, .25, -1, 2
-        y = b0  + b1 * x1 + b2 * x2 + b3 * x1 * x2 + rs.randn(n)
-        df = pd.DataFrame(np.c_[x1, x2, y], columns=["x1", "x2", "y"])
+        #n = 80
+        #rs = np.random.RandomState(11)
+        #x1 = rs.randn(n)
+        #x2 = x1 / 5 + rs.randn(n)
+        #b0, b1, b2, b3 = .5, .25, -1, 2
+        #y = b0  + b1 * x1 + b2 * x2 + b3 * x1 * x2 + rs.randn(n)
+        #df = pd.DataFrame(np.c_[x1, x2, y], columns=["x1", "x2", "y"])
 
         # Show a scatterplot of the predictors with the estimated model surface
-        snw.interactplot(df)
+        #snw.interactplot(df)
