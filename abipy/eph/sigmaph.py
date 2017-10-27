7abef8debab778a941c42fce2cfb4f6642d955ba
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2017-10-27 22:48:20 +0200

    Fix travis again

--- abipy/eph/sigmaph.py
+++ abipy/eph/sigmaph.py
@@ -3,11 +3,180 @@
 This module contains objects for the postprocessing of EPH calculations.
 
 Warning:
-    Work in progress, DO NOT USE IT
+    Work in progress, DO NOT USE THIS CODE
 """
 from __future__ import print_function, division, unicode_literals, absolute_import
 
 import numpy as np
 
-from abipy.core.func1d import Function1D
-from abipy.iotools import ETSF_Reader
+from collections import OrderedDict
+from monty.string import marquee, list_strings
+from monty.functools import lazy_property
+from abipy.core.mixins import AbinitNcFile, Has_Structure, Has_ElectronBands, NotebookWriter
+from abipy.core.kpoints import Kpath
+from abipy.tools.plotting import add_fig_kwargs, get_ax_fig_plt, set_axlims
+from abipy.electrons.ebands import ElectronsReader
+from abipy.dfpt.phonons import PhononBands, factor_ev2units, unit_tag, dos_label_from_units
+
+
+class SigmaPhFile(AbinitNcFile, Has_Structure, Has_ElectronBands, NotebookWriter):
+     """
+     This file contains the Fan-Migdal self-energy, the ElectronBands on the k-mesh.
+     Provides methods to analyze and plot results.
+
+     Usage example:
+
+     .. code-block:: python
+
+         with SigmaPhFile("out_SIGMAPH.nc") as ncfile:
+             print(ncfile)
+             ncfile.ebands.plot()
+     """
+     @classmethod
+     def from_file(cls, filepath):
+         """Initialize the object from a Netcdf file."""
+         return cls(filepath)
+
+     def __init__(self, filepath):
+         super(SigmaPhFile, self).__init__(filepath)
+         self.reader = SigmaPhReader(filepath)
+
+     def __str__(self):
+         """String representation."""
+         return self.to_string()
+
+     def to_string(self, verbose=0):
+         """String representation."""
+         lines = []; app = lines.append
+
+         app(marquee("File Info", mark="="))
+         app(self.filestat(as_string=True))
+         app("")
+         app(marquee("Structure", mark="="))
+         app(str(self.structure))
+         app("")
+         app(self.ebands.to_string(with_structure=False, title="Electronic Bands"))
+         app("")
+         app(marquee("SigmaPh calculation", mark="="))
+
+         #if verbose > 1:
+         #    app(marquee("Abinit Header", mark="="))
+         #    app(self.hdr.to_string(verbose=verbose))
+
+         return "\n".join(lines)
+
+     #@lazy_property
+     #def hdr(self):
+     #    """:class:`AttrDict` with the Abinit header e.g. hdr.ecut."""
+     #    return self.reader.read_abinit_hdr()
+
+     @lazy_property
+     def ebands(self):
+         """:class:`ElectronBands` object."""
+         return self.reader.read_ebands()
+
+     @property
+     def structure(self):
+         """:class:`Structure` object."""
+         return self.ebands.structure
+
+     #@property
+     #def phbands(self):
+     #    """:class:`PhononBands` object with frequencies along the q-path."""
+     #    return self.reader.read_phbands_qpath()
+
+     def close(self):
+         """Close the file."""
+         self.reader.close()
+
+     @add_fig_kwargs
+     def plot(self, what="lambda", units="eV", ylims=None, ax=None, **kwargs):
+         """
+         Plot phonon bands with eph coupling strenght lambda(q, nu) or gamma(q, nu)
+
+         Args:
+             what: `lambda` for eph strength, gamma for ph linewidth.
+             units: Units for phonon plots. Possible values in ("eV", "meV", "Ha", "cm-1", "Thz").
+                 Case-insensitive.
+             scale: float used to scale the marker size.
+             ylims: Set the data limits for the y-axis. Accept tuple e.g. `(left, right)`
+                    or scalar e.g. `left`. If left (right) is None, default values are used
+             ax: matplotlib :class:`Axes` or None if a new figure should be created.
+
+         Returns:
+             `matplotlib` figure
+         """
+         ax, fig, plt = get_ax_fig_plt(ax=ax)
+
+         # Plot phonon bands
+         #self.phbands.plot(ax=ax, units=units, show=False)
+
+         # Add eph coupling.
+         #xvals = np.arange(len(self.phbands.qpoints))
+         #yvals = self.phbands.phfreqs * factor_ev2units(units)
+
+         # [0] is for the number_of_spins
+         # TODO units
+         #if what == "lambda":
+         #    s = self.reader.read_phlambda_qpath()[0]
+         #    scale = 100
+         #elif what == "gamma":
+         #    s = self.reader.read_phgamma_qpath()[0]
+         #    scale = 1
+         #else:
+         #    raise ValueError("Invalid value fo what: `%s`" % what)
+
+         #color = "blue"
+         #for nu in self.phbands.branches:
+         #    ax.scatter(xvals, yvals[:, nu], s=scale * np.abs(s[:, nu]),
+         #            c=color, marker="o", #label=term if ib == 0 else None
+         #    )
+
+         #xvals = np.tile(xvals, 3 * len(self.structure)).T
+         #ax.scatter(xvals.T, yvals.T, s=scale * np.abs(s).T)
+
+         #set_axlims(ax, ylims, "y")
+         #return fig
+
+     def write_notebook(self, nbpath=None):
+         """
+         Write an ipython notebook to nbpath. If nbpath is None, a temporay file in the current
+         working directory is created. Return path to the notebook.
+         """
+         nbformat, nbv, nb = self.get_nbformat_nbv_nb(title=None)
+
+         nb.cells.extend([
+             nbv.new_code_cell("ncfile = abilab.abiopen('%s')" % self.filepath),
+             nbv.new_code_cell("print(ncfile)"),
+             nbv.new_code_cell("ncfile.ebands.plot();"),
+         ])
+
+         return self._write_nb_nbpath(nb, nbpath)
+
+
+ class SigmaPhReader(ElectronsReader):
+     """
+     Reads data from file and constructs objects.
+     """
+
+     def read_phbands_qpath(self):
+         """Read and return PhononBands."""
+         structure = self.read_structure()
+
+         # Build the list of q-points
+         qpoints = Kpath(structure.reciprocal_lattice,
+                         frac_coords=self.read_value("qpath"),
+                         weights=None, names=None, ksampling=None)
+
+         #nctkarr_t('phfreq_qpath', "dp", "natom3, nqpath, number_of_spins"),&
+         phfreqs = self.read_value("phfreq_qpath")[0] * units.Ha_to_eV
+         # TODO
+         phdispl_cart = np.zeros((len(qpoints), 3*len(structure), 3*len(structure)))
+
+         return PhononBands(structure=structure,
+                            qpoints=qpoints,
+                            phfreqs=phfreqs,
+                            phdispl_cart=phdispl_cart,
+                            non_anal_ph=None,
+                            amu=self.read_value("atomic_mass_units"),
+                            )
--- abipy/eph/tests/test_sigmaph.py
+++ abipy/eph/tests/test_sigmaph.py
@@ -8,9 +8,28 @@ import abipy.data as abidata
 from abipy.core.testing import AbipyTest
 
 
-#class EliashbergFunctionTest(AbipyTest):
-#    """Tests for EliashbergFunction."""
+class SigmaPhFileTest(AbipyTest):
 
+    def test_sigmaph_file(self):
+        """Tests for SigmaPhFile."""
+        return
+        ncfile = abilab.abiopen(abidata.ref_file("al_888k_161616q_EPH.nc"))
+        repr(ncfile); str(ncfile)
+        assert ncfile.to_string(verbose=2)
+        assert ncfile.nsppol == 1 and ncfile.nspden == 1 and ncfile.nspinor == 1
+        assert ncfile.ebands.kpoints.is_ibz
+        self.assert_equal(ncfile.ebands.kpoints.ksampling.mpdivs, [8, 8, 8])
+        assert ncfile.phbands.qpoints.is_path
+        assert ncfile.phbands.qpoints.ksampling is None
 
-#class EphReaderTest(AbipyTest):
-#    """Tests for EphReader."""
+        if self.has_matplotlib():
+            # Test A2f plot methods
+            #assert a2f.plot(show=False)
+
+            # Test Eph plot methods.
+            assert ncfile.plot(show=False)
+            assert ncfile.plot_eph_strength(show=False)
+            assert ncfile.plot_with_a2f(show=False)
+
+        if self.has_nbformat():
+            ncfile.write_notebook(nbpath=self.get_tmpname(text=True))
