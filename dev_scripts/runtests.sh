cc01cf7a792c7501835a812bf0e522dccb43813a
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2017-12-29 14:38:01 +0100

    Use ignore opts with pytest

--- dev_scripts/runtests.sh
+++ dev_scripts/runtests.sh
@@ -10,9 +10,11 @@ abicheck.py --with-flow
 # Run unit tests with nose.
 #nosetests -v --with-coverage --cover-package=abipy --logging-level=INFO --doctest-tests
 
-pytest --cov-config=.coveragerc --cov=abipy -v --doctest-modules abipy 
+pytest --cov-config=.coveragerc --cov=abipy -v --doctest-modules abipy \
+    --ignore=abipy/gui --ignore=abipy/gw --ignore=abipy/htc
+
 # This is to run the integration tests (append results)
-# pytest --cov-config=.coveragerc --cov=abipy --cov-append -v abipy/integration_tests
+pytest --cov-config=.coveragerc --cov=abipy --cov-append -v abipy/integration_tests 
 
 # Generate documentation
 if [[ "${PYTHON_VERSION}" == "3.6" && "${TRAVIS_OS_NAME}" == "linux" ]]; then
