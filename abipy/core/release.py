e4a79b7f380109bdd2271d9d6497e31399932546
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2018-06-14 20:37:00 +0200

    Try nomkl

--- abipy/core/release.py
+++ abipy/core/release.py
@@ -30,26 +30,25 @@ min_abinit_version = "8.0.8"
 
 description = "Python package to automate ABINIT calculations and analyze the results."
 
-long_description = \
-    """
-    AbiPy is a Python library to analyze the results produced by `ABINIT <http://www.abinit.org>`_,
-    an open-source program for the ab-initio calculations of the physical properties of materials
-    within Density Functional Theory and Many-Body perturbation theory.
-    AbiPy also provides tools to generate input files and workflows to automate
-    ab-initio calculations and typical convergence studies.
-    AbiPy is interfaced with `Pymatgen <http://www.pymatgen.org>`_ allowing users to
-    benefit from the different tools and python objects available in the pymatgen ecosystem.
+long_description = """\
+AbiPy is a Python library to analyze the results produced by `ABINIT <https://www.abinit.org>`_,
+an open-source program for the ab-initio calculations of the physical properties of materials
+within Density Functional Theory and Many-Body perturbation theory.
+AbiPy also provides tools to generate input files and workflows to automate
+ab-initio calculations and typical convergence studies.
+AbiPy is interfaced with `Pymatgen <http://www.pymatgen.org>`_ allowing users to
+benefit from the different tools and python objects available in the pymatgen ecosystem.
 
-    AbiPy can be used in conjunction with  `matplotlib <http://matplotlib.org>`_, `pandas <http://pandas.pydata.org>`_,
-    `ipython <https://ipython.org/index.html>`_ and `jupyter <http://jupyter.org/>`_
-    thus providing a powerful and user-friendly environment for data analysis and visualization.
-    Check out the list of plotting scripts available in our :doc:`gallery </examples/index>`.
-    To learn more about the integration between jupyter and AbiPy, visit our collection of `notebooks
-    <http://nbviewer.ipython.org/github/abinit/abipy/blob/master/abipy/examples/notebooks/index.ipynb>`_
-    and the `AbiPy lessons <http://nbviewer.ipython.org/github/abinit/abipy/blob/master/abipy/examples/notebooks/lessons/index.ipynb>`_.
+AbiPy can be used in conjunction with  `matplotlib <http://matplotlib.org>`_, `pandas <http://pandas.pydata.org>`_,
+`ipython <https://ipython.org/index.html>`_ and `jupyter <http://jupyter.org/>`_
+thus providing a powerful and user-friendly environment for data analysis and visualization.
+Check out the list of plotting scripts available in our :doc:`gallery </examples/index>`.
+To learn more about the integration between jupyter and AbiPy, visit our collection of `notebooks
+<http://nbviewer.ipython.org/github/abinit/abipy/blob/master/abipy/examples/notebooks/index.ipynb>`_
+and the `AbiPy lessons <http://nbviewer.ipython.org/github/abinit/abipy/blob/master/abipy/examples/notebooks/lessons/index.ipynb>`_.
 
-    The latest development version is always available from <https://github.com/abinit/abipy>
-    """
+The latest development version is always available from <https://github.com/abinit/abipy>
+"""
 
 license = 'GPL'
 
