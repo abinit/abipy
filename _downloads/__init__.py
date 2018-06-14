45e9384816e642060094b86423f5180fc6d6d0e4
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2018-06-15 00:42:31 +0200

    Update documentation

--- _downloads/__init__.py
+++
@@ -1,6 +0,0 @@
-r"""
-AbiPy Flow Gallery
-==================
-
-This gallery contains python scripts to generate AbiPy flows from the command line.
-"""
--- _downloads/__init__1.py
+++
@@ -1,7 +0,0 @@
-r"""
-AbiPy examples
-==============
-
-There are a variety of ways to use AbiPy, and most of them are
-illustrated in the examples in this directory.
-"""
