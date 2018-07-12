7c02aa10aae669aa26f1aef94359c5ee2c0be220
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2018-07-12 23:07:22 +0200

    Deactivate pytest if py3.6 with pip

--- dev_scripts/runtests.sh
+++ dev_scripts/runtests.sh
@@ -11,15 +11,17 @@ abicheck.py --with-flow
 #nosetests -v --with-coverage --cover-package=abipy --logging-level=INFO --doctest-tests
 
 # Run unit tests with pytest. No doctests if 2.7
-if [[ "${TRAVIS_PYTHON_VERSION}" == "2.7" ]]; then 
-    pytest -n 2 --cov-config=.coveragerc --cov=abipy -v abipy \
-	--ignore=abipy/integration_tests --ignore=abipy/data/refs --ignore=abipy/scripts/ \
-	--ignore=abipy/examples/plot --ignore=abipy/examples/flows --ignore=abipy/gui 
-else
-    #pytest --cov-config=.coveragerc --cov=abipy -v --doctest-modules abipy \
-    pytest -n 2 --cov-config=.coveragerc --cov=abipy -v --doctest-modules abipy \
-	--ignore=abipy/integration_tests --ignore=abipy/data/refs --ignore=abipy/scripts/ \
-	--ignore=abipy/examples/plot --ignore=abipy/examples/flows --ignore=abipy/gui 
+if [[ "${ABIPY_PYTEST}" == "yes" ]]; then 
+    if [[ "${TRAVIS_PYTHON_VERSION}" == "2.7" ]]; then 
+	pytest -n 2 --cov-config=.coveragerc --cov=abipy -v abipy \
+	    --ignore=abipy/integration_tests --ignore=abipy/data/refs --ignore=abipy/scripts/ \
+	    --ignore=abipy/examples/plot --ignore=abipy/examples/flows --ignore=abipy/gui 
+    else
+	#pytest --cov-config=.coveragerc --cov=abipy -v --doctest-modules abipy \
+	pytest -n 2 --cov-config=.coveragerc --cov=abipy -v --doctest-modules abipy \
+	    --ignore=abipy/integration_tests --ignore=abipy/data/refs --ignore=abipy/scripts/ \
+	    --ignore=abipy/examples/plot --ignore=abipy/examples/flows --ignore=abipy/gui 
+    fi
 fi
 
 # This is to run the integration tests (append results)
