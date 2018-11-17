2a1f0423e0a4fc93b55059f98790f50f8f581036
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2018-11-17 23:37:20 +0100

    Import workflow machinery take1

--- abipy/flowtk/__init__.py
+++ abipy/flowtk/__init__.py
@@ -5,32 +5,22 @@ import tempfile
 
 from monty.termcolor import cprint
 from pymatgen.io.abinit.abiobjects import *
-#from pymatgen.io.abinit.calculations import *
-from pymatgen.io.abinit.events import EventsParser, autodoc_event_handlers
-#from pymatgen.io.abinit.qadapters import *
-from pymatgen.io.abinit.qadapters import show_qparams, all_qtypes
+from .events import EventsParser, autodoc_event_handlers
+from abipy.flowtk.qadapters import show_qparams, all_qtypes
 from pymatgen.io.abinit.netcdf import NetcdfReader
-from pymatgen.io.abinit.launcher import PyFlowScheduler, PyLauncher
+from abipy.flowtk.launcher import PyFlowScheduler, PyLauncher
 from pymatgen.io.abinit.pseudos import Pseudo, PseudoTable, PseudoParser
-from pymatgen.io.abinit.wrappers import Mrgscr, Mrgddb, Mrggkk, Cut3D  # Fold2Bloch
-try:
-    from pymatgen.io.abinit.wrappers import Fold2Bloch
-except ImportError:
-    pass
-from pymatgen.io.abinit.nodes import Status
-from pymatgen.io.abinit.tasks import *
-from pymatgen.io.abinit.tasks import EphTask
-try:
-    from pymatgen.io.abinit.tasks import ElasticTask
-except ImportError:
-    pass
-from pymatgen.io.abinit.works import *
-from pymatgen.io.abinit.flows import (Flow, G0W0WithQptdmFlow, bandstructure_flow, PhononFlow,
+from abipy.flowtk.wrappers import Mrgscr, Mrgddb, Mrggkk, Cut3D, Fold2Bloch
+from abipy.flowtk.nodes import Status
+from abipy.flowtk.tasks import *
+from abipy.flowtk.tasks import EphTask, ElasticTask
+from abipy.flowtk.works import *
+from abipy.flowtk.flows import (Flow, G0W0WithQptdmFlow, bandstructure_flow, PhononFlow,
     g0w0_flow, phonon_flow, phonon_conv_flow, NonLinearCoeffFlow)
 from pymatgen.io.abinit.abitimer import AbinitTimerParser, AbinitTimerSection
 from pymatgen.io.abinit.abiinspect import GroundStateScfCycle, D2DEScfCycle
 
-from abipy.flowtk.works import *
+#from abipy.flowtk.works import *
 #from abipy.flowtk.gs_works import EosWork
 from abipy.flowtk.dfpt_works import NscfDdksWork, ElasticWork
 
