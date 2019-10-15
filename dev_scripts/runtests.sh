5425a402d9fa58d0f31f0cef97383e486abad7fc
gmatteo <gmatteo@gmail.com>
2019-10-15 14:25:04 +0200

    Minor fixes in travis

--- dev_scripts/runtests.sh
+++ dev_scripts/runtests.sh
@@ -4,15 +4,14 @@ set -e  # exit on first error
 
 echo "PMG_MAPI_KEY: 8pkvwRLQSCVbW2Fe" > ${HOME}/.pmgrc.yaml
 
+echo "Running preliminary tests for Abipy with Abinit..."
 abinit --version
 abinit --build
 abicheck.py --with-flow
 
-# Run unit tests with nose.
-#nosetests -v --with-coverage --cover-package=abipy --logging-level=INFO --doctest-tests
-
 # Run unit tests with pytest. No doctests if 2.7
 if [[ "${ABIPY_PYTEST}" == "yes" ]]; then 
+    echo "Running tests with pytests..."
     if [[ "${TRAVIS_PYTHON_VERSION}" == "2.7" ]]; then 
 	pytest -n 2 --cov-config=.coveragerc --cov=abipy -v abipy \
 	    --ignore=abipy/integration_tests --ignore=abipy/data/refs --ignore=abipy/scripts/ \
@@ -27,11 +26,13 @@ fi
 # This is to run the integration tests (append results)
 # integration_tests are excluded in setup.cfg
 if [[ "${ABIPY_COVERALLS}" == "yes" ]]; then 
+    echo "Running integration tests..."
     pytest -n 2 --cov-config=.coveragerc --cov=abipy --cov-append -v abipy/integration_tests 
 fi
 
 # Generate documentation
 if [[ "${ABIPY_SPHINX}" == "yes" ]]; then
+    echo "Generating documentations with sphinx..."
     pip install -r ./docs/requirements.txt
     cd ./docs && export READTHEDOCS=1 && make && unset READTHEDOCS && cd ..
 fi
