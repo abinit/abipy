1235c22053a456990177408bb22faa88f08f3011
gmatteo <gmatteo@gmail.com>
2019-10-18 14:52:19 +0200

    Install apsscheduler

--- dev_scripts/conda_install.sh
+++ dev_scripts/conda_install.sh
@@ -11,16 +11,16 @@ conda install -y -c abinit abinit=${ABINIT_VERSION}
 abinit --version
 abinit --build
 
+echo "Installing bader executable (http://theory.cm.utexas.edu/henkelman/code/bader/) from matsci ..."
+conda install -y -c matsci bader apscheduler==2.1.0
+
 echo "Installing requirements listed requirements.txt and requirements-optional.txt ..."
 # https://github.com/ContinuumIO/anaconda-issues/issues/542
 conda install -y -c anaconda setuptools
-conda install nomkl
+#conda install nomkl
 conda install -y --file ./requirements.txt
 conda install -y --file ./requirements-optional.txt
 conda install -y -c conda-forge graphviz python-graphviz
-
-echo "Installing bader executable (http://theory.cm.utexas.edu/henkelman/code/bader/) from matsci ..."
-conda install -y -c matsci bader
 
 echo "Testing abinit after pymatgen installation ..."
 #echo "Installing abinit from abinit channel ..."
