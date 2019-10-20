6b10766602139ad963d7c4bc2527211538931e6b
gmatteo <gmatteo@gmail.com>
2019-10-20 22:38:53 +0200

    Fix conda install

--- dev_scripts/conda_install.sh
+++ dev_scripts/conda_install.sh
@@ -12,7 +12,7 @@ abinit --version
 abinit --build
 
 echo "Installing bader executable (http://theory.cm.utexas.edu/henkelman/code/bader/) from matsci ..."
-conda install -y -c matsci bader apscheduler==2.1.0
+conda install -y -c matsci bader
 pip install -c abinit apscheduler==2.1.0
 #pip install apscheduler==2.1.0
 
