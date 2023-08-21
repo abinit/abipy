67ce7891958563eee5d6e9eeb4804018166bad53
Marnik Bercx <mbercx@gmail.com>
2023-08-21 16:10:59 +0200

    Fix `AbinitTimerParseError` import

--- abipy/flowtk/abitimer.py
+++ abipy/flowtk/abitimer.py
@@ -1,3 +1,9 @@
 # flake8: noqa
-#from pymatgen.io.abinit.abitimer import AbinitTimerParserError, AbinitTimerParser, AbinitTimerSection
-from pymatgen.io.abinit.abitimer import AbinitTimerParseError, AbinitTimerParser, AbinitTimerSection
+from pymatgen.core import __version__ as pmg_version
+from pymatgen.io.abinit.abitimer import AbinitTimerParser, AbinitTimerSection
+
+
+if pmg_version < '2023.7.10':
+    from pymatgen.io.abinit.abitimer import AbinitTimerParserError as AbinitTimerParseError
+else:
+    from pymatgen.io.abinit.abitimer import AbinitTimerParseError
