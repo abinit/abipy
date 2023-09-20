19ee49dec4a4bf6cf7ee80fabba82a0d701b0749
Julien Bouquiaux <julien.bouquiaux@hotmail.com>
2023-09-20 18:51:10 +0200

    Clean deltaSCF.py

--- abipy/lumi/deltaSCF.py
+++ abipy/lumi/deltaSCF.py
@@ -6,17 +6,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import math
+from mpmath import coth
 from scipy.integrate import simps
 from scipy.special import factorial
+from abipy.tools.plotting import get_ax_fig_plt, add_fig_kwargs
+
 
 class DeltaSCF():
     """
-    Object to post-process the results from a LumiWork.
+    Object to post-process the results from a LumiWork, following a one-effective phonon mode model (1D-CCM). 
+    For equations, notations and formalism, please refer to : 
+     https://doi.org/10.1103/PhysRevB.96.125132
+     https://doi.org/10.1002/adom.202100649 
     """
 
     @classmethod
     def from_json_file(cls,json_path):
-        """ Create the object from a json file containing the path to netcdf files)"""
+        """ Create the object from a json file containing the path to netcdf files, produced at the end of a LumiWork"""
 
         with open(json_path) as f:
 
@@ -78,7 +84,12 @@ class DeltaSCF():
 
     @classmethod
     def from_four_points_file(cls,filepaths):
-        """ Create the object from a list of netcdf files in the order (Ag,Agstar,Aestar,Ae)"""
+        """ Create the object from a list of netcdf files in the order (Ag,Agstar,Aestar,Ae)
+            Ag     -->  Ground state at relaxed ground state atomic positions
+            Agstar -->  Excited state at relaxed ground state atomic positions
+            Aestar -->  Excited state at relaxed excited state atomic positions
+            Ae     -->  Ground state at relaxed excited state atomic positions
+        """
         energies=[]
         structures=[]
         forces=[]
@@ -155,12 +166,6 @@ class DeltaSCF():
     def structure_ex(self):
         return self.structureex
 
-#    def forces_gs(self):
-#        return self.forces_gs
-
-#    def forces_ex(self):
-#        return self.forces_ex
-
     def natom(self):
         """Number of atoms in structure."""
         return len(self.structuregs)
@@ -172,15 +177,21 @@ class DeltaSCF():
         return (self.structureex.cart_coords - self.structuregs.cart_coords)
 
     def defect_index(self,defect_symbol):
+        """"
+        Defect index in the structure from its symbol, ex defect_index("Eu").
+        """
         index=self.structuregs.get_symbol2indices()[defect_symbol][0]
         return index
 
     def get_dict_per_atom(self,index,defect_symbol):
+        """"
+        Dict. with relevant properties per atom.
+        """
         stru=self.structuregs
         def_index=self.defect_index(defect_symbol=defect_symbol)
         d={}
         d["symbol"]=stru.species[index].name
-        d["mass"]=self.amu_list()[index]
+        d["mass "]=self.amu_list()[index]
         d[r"$\Delta R$"]=(sum(self.diff_pos()[index]**2))**(0.5)
         d[r"$\Delta Q^2$"]=self.amu_list()[index]*sum(self.diff_pos()[index]**2)
         d[r"$\Delta F$"]=(sum(self.forces_gs[index]**2))**(0.5)
@@ -188,8 +199,15 @@ class DeltaSCF():
 
         return d
 
-
     def get_dataframe_atoms(self,defect_symbol):
+        """
+        Panda dataframe with relevant properties per atom.
+        units used are 
+        mass     - amu
+        DeltaR   - Angstrom
+        DeltaQ^2 - amu.Angstrom^2
+        DeltaF   - eV/Angstrom
+        """
         list_of_dict=[]
         for index,atom in enumerate(self.structuregs):
             d=self.get_dict_per_atom(index,defect_symbol)
@@ -197,35 +215,48 @@ class DeltaSCF():
 
         return pd.DataFrame(list_of_dict)
 
-    def get_dict_per_element(self,element):
+    def get_dict_per_specie(self,specie):
         stru=self.structuregs
-        indices=stru.indices_from_symbol(element.name)
-        dr_el=[]
+        indices=stru.indices_from_symbol(specie.name)
+        dr_sp=[]
         for index in indices:
-            dr_el.append(sum(self.diff_pos()[index]**2))
+            dr_sp.append(sum(self.diff_pos()[index]**2))
         d={}
-        d["symbol"]=element.name
-        d["mass"]=element.atomic_mass
-        d[r"$\Delta R^2$"]=sum(dr_el)
-        d[r"$\Delta Q^2$"]=element.atomic_mass*sum(dr_el)
+        d["symbol"]=specie.name
+        d["mass"]=specie.atomic_mass
+        d[r"$\Delta R^2$"]=sum(dr_sp)
+        d[r"$\Delta Q^2$"]=specie.atomic_mass*sum(dr_sp)
 
         return d
-
-
-    def get_dataframe_element(self):
+    
+    def get_dataframe_species(self):
+        """
+        Panda dataframe with relevant properties per species.
+        units used are 
+        mass     - amu
+        DeltaR   - Angstrom
+        DeltaQ^2 - amu.Angstrom^2
+        DeltaF   - eV/Angstrom
+        """
         list_of_dict=[]
-        for index,element in enumerate(self.structuregs.types_of_species):
-            d=self.get_dict_per_element(element)
+        for index,specie in enumerate(self.structuregs.types_of_species):
+            d=self.get_dict_per_specie(specie)
             list_of_dict.append(d)
 
         return pd.DataFrame(list_of_dict)
 
 
     def delta_r(self):
+        """
+        Total Delta_R (Angstrom)
+        """
         d_r_squared=np.sum(self.diff_pos()**2)
         return(np.sqrt(d_r_squared))
 
     def amu_list(self):
+        """
+        List of masses (amu)
+        """
         amu_list=[]
         for atom in self.structuregs.species:
             amu_list.append(atom.atomic_mass)
@@ -233,7 +264,9 @@ class DeltaSCF():
 
     def delta_q(self,unit='atomic'):
         """
-        :return: total Delta_Q in unit of (amu^1/2.Angstrom)
+        Total Delta_Q 
+        Args:
+            unit: amu^1/2.Angstrom if unit = 'atomic', kg^1/2.m if 'SI'
         """
         sq_Q_matrix = np.zeros((self.natom(), 3))
         for a in np.arange(self.natom()):
@@ -247,19 +280,35 @@ class DeltaSCF():
             return(delta_Q)
 
     def effective_mass(self):
+        """
+        Effective mass M
+        """
         M = self.delta_q() ** 2 / self.delta_r() ** 2
         return (M)
 
     def E_zpl(self):
+        """
+        Zero-phonon line energy (eV)
+        """
         return (self.AestarEnergy - self.AgEnergy)
 
     def E_em(self):
+        """
+        Emission energy(eV)
+        """
         return(self.AestarEnergy-self.AeEnergy)
 
     def E_abs(self):
+        """
+        Absorption energy(eV)
+        """
         return(self.AgstarEnergy-self.AgEnergy)
 
     def E_FC_ex(self,unit='eV'):
+        """
+        Franck condon energy in excited state (eV)
+        = Relaxation energy between Ag* and Ae* states
+        """
         e_fc=self.AgstarEnergy - self.AestarEnergy
         if unit == 'SI':
             return 1.602176565e-19*e_fc
@@ -267,6 +316,10 @@ class DeltaSCF():
             return e_fc
 
     def E_FC_gs(self,unit='eV'):
+        """
+        Franck condon energy in ground state (eV)
+        = Relaxation energy between Ae and Ag states
+        """
         e_fc=self.AeEnergy - self.AgEnergy
         if unit == 'SI':
             return 1.602176565e-19*e_fc
@@ -274,11 +327,14 @@ class DeltaSCF():
             return e_fc
 
     def Stoke_shift(self):
+        """
+        Stokes shift (eV)
+        """
         return(self.E_FC_ex()+self.E_FC_gs())
 
     def eff_freq_gs(self):
         """
-        :return: effective frequency of the ground state in eV
+        Phonon effective frequency of the ground state (eV)
         """
         hbar_eV = 6.582119570e-16  # in eV*s
         omega_g=np.sqrt(2*self.E_FC_gs(unit='SI')/(self.delta_q(unit='SI'))**2)
@@ -286,22 +342,33 @@ class DeltaSCF():
 
     def eff_freq_ex(self):
         """
-        :return: effective frequency of the ground state in eV
+        Phonon effective frequency of the excited state (eV)
         """
         hbar_eV = 6.582119570e-16  # in eV*s
         omega_e=np.sqrt(2*self.E_FC_ex(unit='SI')/(self.delta_q(unit='SI'))**2)
         return(hbar_eV*omega_e)
 
     def S_em(self):
+        """
+        Total Huang-Rhys factor for emission following the 1D-CCM
+        """
         S = (self.E_FC_gs()) / (self.eff_freq_gs())
         return (S)
 
     def S_abs(self):
+        """
+        Total Huang-Rhys factor for absorption following the 1D-CCM
+        """
         S = (self.E_FC_ex()) / (self.eff_freq_ex())
         return (S)
 
     def FWHM_1D(self,T=0):
-        from mpmath import coth
+        """
+        Full width at half-maximum following a semi-classical approx in the 1D-CCM 
+        (eq.20-21 of https://doi.org/10.1103/PhysRevB.96.125132)
+        Args:
+            T: Temperature
+        """
         w_0 = np.sqrt(8*np.log(2))*(self.S_em()/np.sqrt(self.S_abs()))*self.eff_freq_gs()
         if T==0:
             return w_0
@@ -310,37 +377,76 @@ class DeltaSCF():
             w_T = w_0 * np.sqrt(coth(self.eff_freq_ex() / (2 * k_b * T)))
             return w_T
 
-    def FC_factor(self):
-        # return FC factor between initial vib state m to final vib state n,
-        # using numerical integration of Hermite polynomials
-
-        return
-
     def FC_factor_approx(self,n):
-        # return FC factor between initial vib state m=0 to final vib state n
-        # approx of same eff frequency in gs and ex
-        # approx of T=0K
+        """
+        FC factor between initial vib. state m=0 to final vib. state n
+        approx of same eff frequency in gs and ex and T = 0K.
+        See eq. (9) of https://doi.org/10.1002/adom.202100649 
+        """
         FC = np.exp(self.S_em()) * self.S_em() ** n / math.factorial(n)
         return FC
 
-    def A_1D_zero_temp(self,width):
+    def lineshape_1D_zero_temp(self,energy_range=[0.5,5],max_m=25,phonon_width=0.01,with_omega_cube="True",normalized='Area'):
+        """
+        Compute the emission lineshape following the effective phonon 1D-CCM at T=0K.
+        See eq. (9) of  https://doi.org/10.1002/adom.202100649 
+        Args:
+            energy_range:  Energy range at which the intensities are computed, ex : [0.5,5]
+            max_m: Maximal vibrational state m considered
+            phonon_width: fwhm of each phonon peak, in eV
+            with_omega_cube: Considered or not the omega^3 dependence of the intensity
+            normlized: Normalisation procedure. 'Area' if Area under the curve = 1
+                       'Sum' if maximum of the curve = 1.
+        Returns:
+            E_x = Energies at which the intensities are computed, np.array
+            I   = Intensities, np.array
+        """
         n_x = 10000  #
-        E_x = np.linspace(1.5, 4, n_x)
-        n_trans = 100  # number of transitions considered
-        m = np.arange(0, n_trans)
-        gaussian_1D = np.zeros((n_trans, n_x))
+        E_x = np.linspace(energy_range[0], energy_range[1], n_x)
 
-        A = np.zeros(n_x)  # A
-        sigma = width / (2.35482)
-        for i in range(n_trans):
-            f=np.exp(self.S_em())*self.S_em()**m[i]/math.factorial(m[i])
-            gaussian_1D[i] = np.absolute(f) * (1 / (sigma * np.sqrt(2 * np.pi))) * np.exp(
-                -((self.E_zpl() - self.eff_freq_gs() * i - E_x) ** 2 / (2 * (sigma) ** 2)))
-            A += gaussian_1D[i]
+        list_n = np.arange(0, max_m)
+        A = np.zeros(n_x) 
+        sigma = phonon_width / (2.35482)
+        for n in list_n:
+            #gaussian_1D = np.zeros(n_x)
+            fc_factor=self.FC_factor_approx(n)
+            #f=np.exp(self.S_em())*self.S_em()**m[i]/math.factorial(m[i])
+            arg_exp=-((self.E_zpl() - self.eff_freq_gs() * n - E_x) ** 2 / (2 * (sigma) ** 2))
+            gaussian_1D = fc_factor * (1 / (sigma * np.sqrt(2 * np.pi))) * np.exp(arg_exp)
+            A += gaussian_1D
 
-        C = 1 / (simps(A, E_x))
+        if with_omega_cube=="True":
+            A=A*E_x**3
 
-        return (E_x, C*A)
+        if normalized=="Area":
+            C = 1 / (simps(A, E_x)) 
+        if normalized=="Sum":
+            C=1/(max(A))
+
+        return E_x, C*A
+    
+    @add_fig_kwargs
+    def plot_lineshape_1D_zero_temp(self,energy_range=[0.5,5],max_m=25,phonon_width=0.01,with_omega_cube="True",
+                                    normalized='Area', ax=None, **kwargs):
+        """
+        Plot the the emission lineshape following the effective phonon 1D-CCM at T=0K.
+        Args:
+        ax: |matplotlib-Axes| or None if a new figure should be created.
+        energy_range:  Energy range at which the intensities are computed, ex : [0.5,5]
+        max_m: Maximal vibrational state m considered
+        phonon_width: fwhm of each phonon peak, in eV
+        with_omega_cube: Considered or not the omega^3 dependence of the intensity
+        normlized: Normalisation procedure. 'Area' if Area under the curve = 1
+                   'Sum' if maximum of the curve = 1.
+        
+        Returns: |matplotlib-Figure|
+        """
+        ax, fig, plt = get_ax_fig_plt(ax=ax)
+
+        x,y=self.lineshape_1D_zero_temp(energy_range,max_m,phonon_width,with_omega_cube,
+                                        normalized)
+        ax.plot(x,y, **kwargs)
+        return fig
 
     def get_dict_results(self):
         d=dict([
@@ -359,11 +465,11 @@ class DeltaSCF():
         ])
         return d
 
-
     def get_dataframe(self,label=None):
         """
-        :return: pd dataframe with the main results of a lumi work : transition energies, delta Q, Huang Rhys factor,...
-
+        Panda dataframe with the main results of a LumiWork : transition energies, delta Q, Huang Rhys factor,...
+        Units used are Angstrom, eV, amu. 
+        DeltaSCF object should be instantiated with the four points files, not with relax files only. 
         """
         rows=[]
         index=[]
@@ -374,8 +480,17 @@ class DeltaSCF():
         df=pd.DataFrame(rows,index=index)
 
         return df
-
-    def displacements_visu(self):
+    
+    @add_fig_kwargs
+    def displacements_visu(self,a_g=10,**kwargs):
+        """
+        Make a 3d visualisation of the displacements induced by the electronic transition =
+        Difference between ground state and excited state atomic positions.
+        The colors of the atoms are based on Delta_Q_^2 per atom.
+        Args:
+            a_g = coefficient that multiplies the displacement magnitudes
+        Returns: |matplotlib-Figure|
+        """
         pos_gs=self.structuregs.cart_coords
         pos_ex=self.structureex.cart_coords
         # make the grid
@@ -390,21 +505,29 @@ class DeltaSCF():
 
         M = self.amu_list()*(u**2+v**2+w**2)
 
-        fig = plt.figure()
+        fig=plt.figure()
         ax = fig.add_subplot(111, projection='3d')
 
-        a_g=10 ### vector size increase coef
         ax.quiver(x, y, z,u*a_g,v*a_g,w*a_g, color='k',linewidths=1)
         sc = ax.scatter(x, y, z, c=M, marker='o', s=60, cmap="jet")
-        #ax.scatter(self.pos_gs[0, 0], self.pos_gs[0, 1], self.pos_gs[0, 2], marker='o', s=200, color='fuchsia')
 
-        #set plot to scale
-       # a=0.37
-       # ax.get_proj = lambda: np.dot(Axes3D.get_proj(ax), np.diag([1*a,1*a,(90/15)*a,1]))
         clb=plt.colorbar(sc)
         clb.set_label(r'$\Delta Q^2$ per atom')
+        return fig
 
-    def plot_dr_distance(self,defect_symbol,colors=["k","r","g","b","c","m"]):
+
+    @add_fig_kwargs
+    def plot_delta_R_distance(self, defect_symbol,colors=["k","r","g","b","c","m"],ax=None, **kwargs):
+        """
+        Plot \DeltaR vs distance from defect for each atom, colored by species.
+        Args:
+        ax: |matplotlib-Axes| or None if a new figure should be created.
+        defect_symbol:  defect_symbol, defect location will be the reference 
+        colors: list of colors for the species
+        
+        Returns: |matplotlib-Figure|
+        """
+
         symbols=self.structuregs.symbol_set
         dfs = []
         xs = []
@@ -415,18 +538,28 @@ class DeltaSCF():
             dfs.append(df.loc[df['symbol'] == symbol])
             xs.append(dfs[i]["dist. from defect"])
             ys.append(dfs[i]["$\\Delta R$"])
-
-        f = plt.figure()
-
+        
+        ax, fig, plt = get_ax_fig_plt(ax=ax)
         for i, symbol in enumerate(symbols):
-            plt.stem(xs[i], ys[i], label=symbol, linefmt=colors[i], markerfmt="o" + colors[i])
-            plt.xlabel(r'Distance from defect ($\AA$)')
-            plt.ylabel(r'$\Delta R $ ($\AA$)')
-            plt.legend()
+            ax.stem(xs[i], ys[i], label=symbol, linefmt=colors[i], markerfmt="o" + colors[i],**kwargs)
+            ax.set_xlabel(r'Distance from defect ($\AA$)')
+            ax.set_ylabel(r'$\Delta R $ ($\AA$)')
+            ax.legend()
+        
+        return fig
+    
+    @add_fig_kwargs
+    def plot_delta_F_distance(self, defect_symbol,colors=["k","r","g","b","c","m"],ax=None, **kwargs):
+        """
+        Plot \DeltaF vs distance from defect for each atom, colored by species.
+        Args:
+        ax: |matplotlib-Axes| or None if a new figure should be created.
+        defect_symbol:  defect_symbol, defect location will be the reference 
+        colors: list of colors for the species
+        
+        Returns: |matplotlib-Figure|
+        """
 
-        return f
-
-    def plot_dF_distance(self,defect_symbol,colors=["k","r","g","b","c","m"]):
         symbols=self.structuregs.symbol_set
         dfs = []
         xs = []
@@ -437,17 +570,17 @@ class DeltaSCF():
             dfs.append(df.loc[df['symbol'] == symbol])
             xs.append(dfs[i]["dist. from defect"])
             ys.append(dfs[i]["$\\Delta F$"])
-
-        f = plt.figure()
-
+        
+        ax, fig, plt = get_ax_fig_plt(ax=ax)
         for i, symbol in enumerate(symbols):
-            plt.stem(xs[i], ys[i], label=symbol, linefmt=colors[i], markerfmt="o" + colors[i])
-            plt.xlabel(r'Distance from defect ($\AA$)')
-            plt.ylabel(r'$\Delta F$ ($eV/\AA$)')
-            plt.legend()
-
-        return f
-
+            ax.stem(xs[i], ys[i], label=symbol, linefmt=colors[i], markerfmt="o" + colors[i],**kwargs)
+            ax.set_xlabel(r'Distance from defect ($\AA$)')
+            ax.set_ylabel(r'$\Delta F$ ($eV/\AA$)')
+            ax.legend()
+        
+        return fig
+    
+    @add_fig_kwargs
     def plot_four_BandStructures(self,nscf_files):
         """"
         plot the 4 band structures
@@ -469,12 +602,3 @@ class DeltaSCF():
             ax.label_outer()
 
         plt.subplots_adjust(hspace=0.5, wspace=0.1)
-
-
-
-
-
-
-
-
-
