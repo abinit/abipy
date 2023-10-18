82f4978f173dad2864e688e3145c3a427c0cb822
SurgeArrester <cameron.h@rgreaves.me.uk>
2023-10-18 15:19:00 +0200

    updated upstream changes

--- abipy/flowtk/abitimer.py
+++ abipy/flowtk/abitimer.py
@@ -3,7 +3,10 @@ from pymatgen.core import __version__ as pmg_version
 from pymatgen.io.abinit.abitimer import AbinitTimerParser, AbinitTimerSection
 
 
-<<<<<<< HEAD
-=======
 #print(f"{pmg_version=}")
 #if pmg_version < '2023.7.10':
+
+try:
+    from pymatgen.io.abinit.abitimer import AbinitTimerParserError as AbinitTimerParseError
+except ImportError:
+    from pymatgen.io.abinit.abitimer import AbinitTimerParseError
