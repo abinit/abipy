9f2221689605181dda9bf8cdebe12236dfebeaa3
Samare <samare.rostami@gmail.com>
2025-03-05 17:51:55 +0100

    ZSISA (#317)
    * deformation_utils has been modified based on the paper.
    
    * qha_general_stress is modified base on the paper.
    
    * the test for qha_general stress must change.
    
    * some modification in qha_general_stress
    
    * test  is modified.

--- abipy/dfpt/tests/test_deformation_utils.py
+++ abipy/dfpt/tests/test_deformation_utils.py
@@ -10,11 +10,11 @@ class DeformationUtilsTest(AbipyTest):
 
     def test_generate_deformations(self):
         """Testing generate_deformations"""
-        eps = 0.005
+        eps = 0.005/1.005
         si = self.get_structure("Si")
         structures_dict, strain_inds, spgrp_number = generate_deformations(si, eps)
         assert len(structures_dict) == 3
         assert spgrp_number == 227
         assert strain_inds.shape == (3, 6)
-        self.assert_equal(strain_inds[:, 0], [0, 1, 2])
-        assert np.all(strain_inds[1:, 1] == 0)
+        self.assert_equal(strain_inds[:, 0], [0, -1, 1])
+        assert np.all(strain_inds[3:, 1] == 0)
